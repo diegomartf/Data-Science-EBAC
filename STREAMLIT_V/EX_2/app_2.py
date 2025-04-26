397b54e50eaf498d3640b855d8a4bb7a79bbb6c2
diegomartf <diegomartinsa15@gmail.com>
2025-04-26 14:19:58 -0300

    app

--- STREAMLIT_V/EX_2/app_2.py
+++ STREAMLIT_V/EX_2/app_2.py
@@ -177,7 +177,7 @@ def main():
     with st.echo():
         ""
         # Ler o arquivo CSV 'online_shoppers_intention.csv' e armazenar os dados em um DataFrame chamado df
-        df = pd.read_csv('https://raw.githubusercontent.com/rhatiro/Curso_EBAC-Profissao_Cientista_de_Dados/main/Modulo_31_-_Streamlit_V/Exercicio_2/online_shoppers_intention.csv')
+        df = pd.read_csv('https://raw.githubusercontent.com/diegomartf/Data-Science-EBAC/refs/heads/main/STREAMLIT_V/EX_2/online_shoppers_intention.csv')
 
         # Exibir o DataFrame df, mostrando os dados carregados do arquivo CSV
         st.dataframe(df)
